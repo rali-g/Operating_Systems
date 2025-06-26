160a8cd2eedb7af42ad6e8dd2b6c720ef7603797
Ralitsa Gercheva <138673469+rali-g@users.noreply.github.com>
2025-06-26 18:01:06 +0300

    Update main.c

--- c/processes/2016-SE-02/main.c
+++ c/processes/2016-SE-02/main.c
@@ -18,7 +18,7 @@ int main(void) {
         if(res == -1){
             err(2, "read error");
         }
-        buff[res - 1] = '\0';
+        buff[res] = '\0';
         
         if(strcmp(buff, "exit") == 0){
             exit(0);
